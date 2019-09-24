8a3847c4401a10d94a56bb26f3350ec473758568
tulxoro <thedarkcrusaders@outlook.com>
2019-09-24 08:09:30 -0700

    Converted layout

--- app/src/main/java/com/example/tictactoe_java_scrum/MainActivity.java
+++ app/src/main/java/com/example/tictactoe_java_scrum/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tictactoe_java_scrum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imgView = findViewById(R.id.background);
+        imgView.setImageResource(R.drawable.tic_tac_toe);
     }
 }
