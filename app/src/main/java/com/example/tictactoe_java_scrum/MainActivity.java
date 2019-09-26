b6a7c7b66cfa17d235b5e8c78065acf5ecf51170
tulxoro <thedarkcrusaders@outlook.com>
2019-09-26 08:34:03 -0700

    Added buttons

--- app/src/main/java/com/example/tictactoe_java_scrum/MainActivity.java
+++ app/src/main/java/com/example/tictactoe_java_scrum/MainActivity.java
@@ -3,11 +3,12 @@ package com.example.tictactoe_java_scrum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -15,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
         // sets background of the app
         ImageView imgView = findViewById(R.id.background);
         imgView.setImageResource(R.drawable.tic_tac_toe);
+
+        Button reset = findViewById(R.id.button_reset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // reset board
+            }
+        });
+
     }
 }
