89bdd4fc8326288f1c7de920530818ead2e00687
tulxoro <thedarkcrusaders@outlook.com>
2019-09-27 08:28:01 -0700

    Added toast to reset button

--- app/src/main/java/com/example/tictactoe_java_scrum/MainActivity.java
+++ app/src/main/java/com/example/tictactoe_java_scrum/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,9 +21,19 @@ public class MainActivity extends AppCompatActivity {
         Button reset = findViewById(R.id.button_reset);
         reset.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // reset board
+                Toast toast = Toast.makeText(getApplicationContext(), "The board has been reset!", Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
-
     }
 }
+/*.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+
+                        "Color Changed to R: "+r_value+", G: "+g_value+", B: " +b_value,
+                        Toast.LENGTH_SHORT);
+                Log.i("toast", "Toast has Been Generated");
+                toast.show();
+            }
+        });*/
